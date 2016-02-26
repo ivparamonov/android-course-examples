350c9e0bb60d184af7a6ee0b869370bcabc0a962
Ilya Paramonov <ilya.paramonov@fruct.org>
2016-02-26 22:36:45 +0300

    Introduce Butter Knife for view-related bindings

--- todo-list/app/src/main/java/ru/ac/uniyar/todolist/MainActivity.java
+++ todo-list/app/src/main/java/ru/ac/uniyar/todolist/MainActivity.java
@@ -7,22 +7,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+    @OnClick(R.id.fab)
+    public void onFABClick(View view) {
+        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
-            }
-        });
     }
 }
