7628be70768ff50df8f184787b2eaa73d6319193
Ilya Paramonov <ilya.paramonov@fruct.org>
2016-11-27 14:43:22 +0300

    Add the MediaPlayerService example project

---
+++ media-player-service/app/src/main/java/ru/ac/uniyar/mediaplayerservice/MainActivity.java
@@ -0,0 +1,28 @@
+package ru.ac.uniyar.mediaplayerservice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.startButton).setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startService(new Intent(MainActivity.this, MediaPlayerService.class));
+            }
+        });
+        findViewById(R.id.stopButton).setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopService(new Intent(MainActivity.this, MediaPlayerService.class));
+            }
+        });
+    }
+}
---
+++ media-player-service/app/src/main/java/ru/ac/uniyar/mediaplayerservice/MediaPlayerService.java
@@ -0,0 +1,37 @@
+package ru.ac.uniyar.mediaplayerservice;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class MediaPlayerService extends Service {
+    private MediaPlayer mediaPlayer;
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.examplesound);
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        mediaPlayer.stop();
+        super.onDestroy();
+    }
+}
